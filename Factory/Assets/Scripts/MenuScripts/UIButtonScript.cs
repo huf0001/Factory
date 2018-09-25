a476ce65966bf42962786bb7503a09b68ed32b5c
DESKTOP-5S357S1\sarah <101265321@student.swin.edu.au>
2018-09-25 16:57:41 +1000

    Added placement scenes for build so the UI can be done: instructions and menu script UI buttons done: can be used in other scenes to preserve the music toggling just attach to main camera

---
+++ Factory/Assets/Scripts/MenuScripts/UIButtonScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtonScript : MonoBehaviour {
+    public AudioSource menuAudio;
+    Scene currentScene;
+    // Use this for initialization
+    void Start() {
+        currentScene = SceneManager.GetActiveScene();
+        if ((currentScene.name == "MainMenu")&&(PlayerPrefs.GetString("music") == null)) { PlayerPrefs.SetString("music", "true"); }
+          
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if ((PlayerPrefs.GetString("music"))=="true")
+        {
+            menuAudio.mute = false;
+        }
+        else { menuAudio.mute = true; }
+    }
+
+    public void ToggleMusic() {
+        if ((PlayerPrefs.GetString("music") == "true"))
+        {
+            PlayerPrefs.SetString("music", "false");
+        }
+        else { PlayerPrefs.SetString("music", "true"); }
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+}
