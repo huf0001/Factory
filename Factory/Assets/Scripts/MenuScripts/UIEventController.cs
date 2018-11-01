3374adcc62b5268726f29d21e19b96ecf3fd0332
huf0001 <samhuffer01@gmail.com>
2018-11-02 08:56:43 +1100

    SourceTree being a pain

--- Factory/Assets/Scripts/MenuScripts/UIEventController.cs
+++ Factory/Assets/Scripts/MenuScripts/UIEventController.cs
@@ -1,16 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class UIEventController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //[SerializeField] private Gamepad gamepad = Gamepad.XboxController;
+    [SerializeField] private EventSystem DSeventSystem;
+    [SerializeField] private EventSystem XboxeventSystem;
+    [SerializeField] private GameObject DSeventSystemObject;
+    [SerializeField] private GameObject XboxeventSystemObject;
+    [SerializeField] private GameObject firstSelected;
+
+    private bool buttonSelected;
+
+    // Use this for initialization
+    void Start () {
+        string gamepadPreference = "";
+        gamepadPreference = PlayerPrefs.GetString("gamepad");
+        DSeventSystemObject.SetActive(false);
+        XboxeventSystemObject.SetActive(false);
+        if (gamepadPreference == "dualshock")
+        {
+            //gamepad = Gamepad.DualshockController;
+            if (Input.GetAxis("UIDSVertical") != 0 && buttonSelected == false) {
+                DSeventSystemObject.SetActive(true);
+                DSeventSystem.SetSelectedGameObject(firstSelected);
+                buttonSelected = true;
+            }
+        }
+        else
+        {
+            //gamepad = Gamepad.XboxController;
+            if (Input.GetAxis("UIXboxVertical") != 0 && buttonSelected == false)
+            {
+                XboxeventSystemObject.SetActive(true);
+                XboxeventSystem.SetSelectedGameObject(firstSelected);
+                buttonSelected = true;
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+
 }
